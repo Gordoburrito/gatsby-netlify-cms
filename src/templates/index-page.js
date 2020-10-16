863799261482c2ae1d624371e53b46db4078ba25
Gordon Lewis <glewis@berkeley.edu>
2020-10-16 12:54:38 -0700

    added a title section to blurbs. Process: change config.yml , add content to the field in the CMS, check the query in graphiql, put in the query into component, (put in the component?), restart the dev server. maybe npm run start but npm run build + npm run start works well.

--- src/templates/index-page.js
+++ src/templates/index-page.js
@@ -33,7 +33,6 @@ export const IndexPageTemplate = ({
             >
               {title}
               {contactInfo.phone}
-              
             </h1>
             <h3
               className="has-text-weight-bold is-size-5-mobile is-size-5-tablet is-size-4-widescreen"
@@ -178,6 +177,7 @@ export const pageQuery = graphql`
               }
             }
             text
+            title
           }
           heading
           description

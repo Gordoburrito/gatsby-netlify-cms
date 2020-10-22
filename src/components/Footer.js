8edc50aa42a6e2ea4b75415d2df4a4332ddbcc7e
Gordon Lewis <glewis@berkeley.edu>
2020-10-22 10:49:46 -0700

    added initial styling to footer, navbar goes up and down on scroll, fixed slight hickup in styling of feature component, changed name of company and about files

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -10,99 +10,33 @@ import vimeo from '../img/social/vimeo.svg'
 const Footer = class extends React.Component {
   render() {
     return (
-      <footer className="footer has-background-black has-text-white-ter">
-        <div className="content has-text-centered">
-          <img
-            src={logo}
-            alt="Kaldi"
-            style={{ width: '14em', height: '10em' }}
-          />
-        </div>
-        <div className="content has-text-centered has-background-black has-text-white-ter">
-          <div className="container has-background-black has-text-white-ter">
+      <footer className="footer">
+        <div className="content">
+          <div className="container">
             <div style={{ maxWidth: '100vw' }} className="columns">
+              <div className="column is-6 is-offset-1">
+                <h1>newsletter form goes here</h1>
+              </div>
               <div className="column is-4">
                 <section className="menu">
-                  <ul className="menu-list">
+                  <ul className="footer__menu-list">
                     <li>
-                      <Link to="/" className="navbar-item">
-                        Home
+                      <Link className="footer__navbar-item" to="/about">
+                        Company
                       </Link>
                     </li>
                     <li>
-                      <Link className="navbar-item" to="/about">
-                        About
+                      <Link className="footer__navbar-item" to="/products">
+                        Services
                       </Link>
                     </li>
                     <li>
-                      <Link className="navbar-item" to="/products">
-                        Products
-                      </Link>
-                    </li>
-                    <li>
-                      <Link className="navbar-item" to="/contact/examples">
-                        Form Examples
-                      </Link>
-                    </li>
-                    <li>
-                      <a
-                        className="navbar-item"
-                        href="/admin/"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                      >
-                        Admin
-                      </a>
-                    </li>
-                  </ul>
-                </section>
-              </div>
-              <div className="column is-4">
-                <section>
-                  <ul className="menu-list">
-                    <li>
-                      <Link className="navbar-item" to="/blog">
-                        Latest Stories
-                      </Link>
-                    </li>
-                    <li>
-                      <Link className="navbar-item" to="/contact">
-                        Contact
+                      <Link className="footer__navbar-item" to="/blog">
+                        Blog
                       </Link>
                     </li>
                   </ul>
                 </section>
-              </div>
-              <div className="column is-4 social">
-                <a title="facebook" href="https://facebook.com">
-                  <img
-                    src={facebook}
-                    alt="Facebook"
-                    style={{ width: '1em', height: '1em' }}
-                  />
-                </a>
-                <a title="twitter" href="https://twitter.com">
-                  <img
-                    className="fas fa-lg"
-                    src={twitter}
-                    alt="Twitter"
-                    style={{ width: '1em', height: '1em' }}
-                  />
-                </a>
-                <a title="instagram" href="https://instagram.com">
-                  <img
-                    src={instagram}
-                    alt="Instagram"
-                    style={{ width: '1em', height: '1em' }}
-                  />
-                </a>
-                <a title="vimeo" href="https://vimeo.com">
-                  <img
-                    src={vimeo}
-                    alt="Vimeo"
-                    style={{ width: '1em', height: '1em' }}
-                  />
-                </a>
               </div>
             </div>
           </div>
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,21 +1,47 @@
-import React from 'react'
-import { Link } from 'gatsby'
-import github from '../img/github-icon.svg'
-import logo from '../img/logo.svg'
-import instagram from '../img/instagram.svg'
-import facebook from '../img/facebook.svg'
-import tiktok from '../img/tiktok.svg'
-
+import React from "react";
+import { Link } from "gatsby";
+import github from "../img/github-icon.svg";
+import logo from "../img/logo.svg";
+import instagram from "../img/instagram.svg";
+import facebook from "../img/facebook.svg";
+import tiktok from "../img/tiktok.svg";
 
 const Navbar = class extends React.Component {
   constructor(props) {
-    super(props)
+    super(props);
     this.state = {
       active: false,
-      navBarActiveClass: '',
-    }
+      navBarActiveClass: "",
+      prevScrollpos: window.pageYOffset,
+      navBarVisibleClass: true,
+    };
+  }
+  handleScroll =() =>{
+    const { prevScrollpos } = this.state;
+
+    const currentScrollPos = window.pageYOffset;
+    const visible = prevScrollpos > currentScrollPos;
+
+    visible
+    ? this.setState({
+        navBarVisibleClass: "",
+      })
+    : this.setState({
+        navBarVisibleClass: "navbar--hidden",
+      });
+
+    this.setState({
+      prevScrollpos: currentScrollPos,
+    });
   }
 
+  componentDidMount () {
+    window.addEventListener("scroll", this.handleScroll);
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener("scroll", this.handleScroll);
+  }
   toggleHamburger = () => {
     // toggle the active boolean in the state
     this.setState(
@@ -27,27 +53,23 @@ const Navbar = class extends React.Component {
         // set the class in state for the navbar accordingly
         this.state.active
           ? this.setState({
-              navBarActiveClass: 'is-active',
+              navBarActiveClass: "is-active",
             })
           : this.setState({
-              navBarActiveClass: '',
-            })
+              navBarActiveClass: "",
+            });
       }
-    )
-  }
-//mix-blend-mode: exclusion;
-//took out mix bar you should push as well!
+    );
+  };
+  //mix-blend-mode: exclusion;
+  //took out mix bar you should push as well!
   render() {
     return (
-      <nav
-        className="navbar"
-        role="navigation"
-        aria-label="main-navigation"
-      >
+      <nav className={`navbar ${this.state.navBarVisibleClass}`} role="navigation" aria-label="main-navigation">
         <div className="container">
           <div className="navbar-brand">
             <Link to="/" className="navbar-item" title="Logo">
-              <img src={logo} alt="Kaldi" style={{ width: '88px' }} />
+              <img src={logo} alt="TrendZ" style={{ width: "88px" }} />
             </Link>
             {/* Hamburger menu */}
             <div
@@ -64,12 +86,14 @@ const Navbar = class extends React.Component {
             id="navMenu"
             className={`navbar-menu ${this.state.navBarActiveClass}`}
           >
-            <div className="navbar-start has-text-centered">
-              <Link className="navbar-item" to="/about">
-                About
+            {/* <div className="nav navbar-start has-text-centered">
+            </div> */}
+            <div className="navbar-end has-text-centered">
+              <Link className="navbar-item" to="/company">
+                Company
               </Link>
               <Link className="navbar-item" to="/products">
-                Products
+                Services
               </Link>
               <Link className="navbar-item" to="/blog">
                 Blog
@@ -77,21 +101,6 @@ const Navbar = class extends React.Component {
               <Link className="navbar-item" to="/contact">
                 Contact
               </Link>
-              <Link className="navbar-item" to="/contact/examples">
-                Form Examples
-              </Link>
-            </div>
-            <div className="navbar-end has-text-centered">
-              <a
-                className="navbar-item"
-                href="https://github.com/netlify-templates/gatsby-starter-netlify-cms"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                <span className="icon">
-                  <img src={github} alt="Github" />
-                </span>
-              </a>
               <a
                 className="navbar-item"
                 href="https://github.com/netlify-templates/gatsby-starter-netlify-cms"
@@ -126,8 +135,8 @@ const Navbar = class extends React.Component {
           </div>
         </div>
       </nav>
-    )
+    );
   }
-}
+};
 
-export default Navbar
+export default Navbar;
